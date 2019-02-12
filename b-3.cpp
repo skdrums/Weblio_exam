467b7d9b2d8fdf40922a3aa7c683fb0a6e8c64ec
Sora Kicuchi <sora.kannaduki1009@gmail.com>
2019-02-12 18:01:31 +0900

    edit the file b-3.cpp

--- b-3.cpp
+++ b-3.cpp
@@ -1,55 +1,27 @@
-#include <stdio.h>
-#include <iostream>
-#include <stdlib.h>
-#include <string.h>
-using namespace std;
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;      
 #define N 10
+#define SENTINEL 1000000000
 
 class SortCollections{
     public:
-        static void bubbleSort(int[]);
-        static void mergeSort(int[]);
-        static void merge(int[]);
-        static void quicksort(int[]);
-        static void sort(int[]);
-}
+        static void mergeSort(int A[]);
+        static void mergeSort(int A[],int left,int right);
+        static void merge(int A[],int left,int mid,int right);
+        static void bubbleSort(int a[]);
+        static void quickSort(int a[]);
+        static void quickSort(int a[],int left,int right);
+        static void sort(int a[]);
+
+};
 
 
-static void bubbleSort(int a[N]){
-
-        int s;
-        for(int i = 0; i < N-1; i++)
-        {
-        for(int j = N-1; j > i; j--)
-            {
-                if(a[j]<a[j-1]){
-                    s=j;
-                    a[j]=a[j-1];
-                    a[j-1]=a[s];
-                }
-            }
-        }
-
-}
-
-static void mergeSort(int a[N]){
-    int i,mid,left,right;
-    left=0;right=sizeof(a)/sizeof(a[0]);
-    if(left+1<right){
-        mid=(left+right)/2;
-        mergeSort(a);
-        mergeSort(a);
-        merge(a);
-    }
-
-}
-
-static void merge(int A[N]){
-  int n1,n2,i,j,k,count,left,right,mid;
+void SortCollections::merge(int A[],int left,int mid,int right){
+  int count=0;
+  int n1,n2,i,j,k;
   int *L,*R;
-  count=0;left=0;right=sizeof(a)/sizeof(a[0]);
-  mid=(right+left)/2;
   n1=mid-left;
   n2=right-mid;
   L=(int *)malloc(sizeof(int)*(n1+1));
@@ -80,38 +52,89 @@ static void merge(int A[N]){
   free(R);
 }
 
+void SortCollections::mergeSort(int A[],int left,int right){
+  int i,mid;
+  if(left+1<right){
+    mid=(left+right)/2;
+    mergeSort(A,left,mid);
+    mergeSort(A,mid,right);
+    merge(A,left,mid,right);
+  }   
+}
 
-static void quicksort(int a[N])
+void SortCollections::mergeSort(int A[]){
+    int mid, left, right;
+    left=0;right=N;
+    if(left+1<right){
+        mid=(left+right)/2;
+        mergeSort(A,left,mid);
+        mergeSort(A,mid,right);
+        merge(A,left,mid,right);
+    }
+}
+
+void SortCollections::bubbleSort(int a[]){
+
+  int s;
+  for(int i = 0; i < N-1; i++){
+    for(int j = N-1; j > i; j--){
+      if(a[j]<a[j-1]){
+        s=a[j];
+        a[j]=a[j-1];
+        a[j-1]=s;
+      }
+    }
+  }
+
+}
+
+void SortCollections::quickSort(int a[])
 {
-    int left,right;
-    left=0;right=sizeof(a)/sizeof(a[0]);
+    int left,right,s,tmp;
+    left=0;right=N-1;
     if (left < right) {
-        int i = left, j = right;
-        value_type tmp, pivot = med3(a[i], a[i + (j - i) / 2], a[j]); /* (i+j)/2ではオーバーフローしてしまう */
+      s=a[(left+right)/2];
+        int i = left-1, j = right+1;
         while (1) { /* a[] を pivot 以上と以下の集まりに分割する */
-            while (a[i] < pivot) i++; /* a[i] >= pivot となる位置を検索 */
-            while (pivot < a[j]) j--; /* a[j] <= pivot となる位置を検索 */
+            while (a[++i]<s); /* a[i] >= pivot となる位置を検索 */
+            while (s < a[--j]) ; /* a[j] <= pivot となる位置を検索 */
             if (i >= j) break;
             tmp = a[i]; a[i] = a[j]; a[j] = tmp; /* a[i],a[j] を交換 */
             i++; j--;
         }
-        quicksort(a, left, i - 1);  /* 分割した左を再帰的にソート */
-        quicksort(a, j + 1, right); /* 分割した右を再帰的にソート */
+        quickSort(a, left, i - 1);  /* 分割した左を再帰的にソート */
+        quickSort(a, j + 1, right); /* 分割した右を再帰的にソート */
     }
 
 }
 
-static void sort(int a[N]){
+void SortCollections::quickSort(int a[],int left,int right)
+{
+    int tmp,s,i,j;
+    if (left < right) {
+        s=a[(left+right)/2];
+        i = left-1, j = right+1;
+        while (1) { /* a[] を pivot 以上と以下の集まりに分割する */
+            while (a[++i] < s); /* a[i] >= pivot となる位置を検索 */
+            while (s < a[--j]); /* a[j] <= pivot となる位置を検索 */
+            if (i >= j) break;
+            tmp = a[i]; a[i] = a[j]; a[j] = tmp; /* a[i],a[j] を交換 */
+        }
+        quickSort(a, left, i - 1);  /* 分割した左を再帰的にソート */
+        quickSort(a, j + 1, right); /* 分割した右を再帰的にソート */
+    }
 
 }
 
-int main(void){
-    int a[]={80,40,35,12,88,1,56,33,50,23};
+void SortCollections::sort(int a[]){
 
-    SortCollections::quicksort(a);
-    for(int i = 0; i < N; i++)
-    {
-        cout<<a[i]<<" ";
-    }
-    
+}
+
+
+int main(){
+    int A[10]={80,40,35,12,88,1,56,33,50,23};
+    SortCollections::quickSort(A);
+    for(int i=0;i<10;i++) cout<<A[i]<<" ";
+
+
 }
