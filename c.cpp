bcc2694e4c1128f77104fe4a10d28428cb2cfe57
Sora Kicuchi <sora.kannaduki1009@gmail.com>
2019-02-19 23:37:44 +0900

    add new file c.cpp and c.html

---
+++ c.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<random>
+#include <math.h>
+#include <cmath>
+using namespace std;  
+
+
+int mainl(void){
+
+    //fileの読み込み
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
