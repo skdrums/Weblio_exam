366e07b64fa37b94baa3c0756ddb1cad4a90d738
Sora Kicuchi <sora.kannaduki1009@gmail.com>
2019-02-19 23:19:03 +0900

    problem b-2 is in progress.

--- b-2.cpp
+++ b-2.cpp
@@ -16,6 +16,11 @@ struct Vertex2D{
 	double y;
 };
 
+struct min_triangle{
+    double min;
+    Vertex2D dot[4];
+}; 
+
 //ベクトルの定義(頂点と同じ)
 #define Vector2D Vertex2D
 
@@ -32,6 +37,8 @@ double cross_vector(Vector2D vl, Vector2D vr) {
 //点Pと線(AB)の距離
 double Distance_DotAndLine(Vertex2D, Vertex2D, Vertex2D);
 double Distance_DotAndDot(Vertex2D,Vertex2D);
+double Area_Triangle(Vertex2D,Vertex2D,Vertex2D);
+void CmpEachSide(double a[]);
 
 int main(void){
 
@@ -41,11 +48,7 @@ int main(void){
         Vertex2D end;
     } min_line[10]={0,{0,0},{0,0}};
 
-    struct min_triangle{
-        Vertex2D dot_one;
-        Vertex2D dot_two;
-        Vertex2D dot_three;
-    } min_tri[10]={{0,0},{0,0},{0,0}};
+    min_triangle min_tri[10]={{0,0},{0,0},{0,0}};
 
     Vertex2D z[M];
     double m,n;
@@ -82,10 +85,10 @@ int main(void){
             }
         }
     }
-    int flag;
+    int line_flag;
     for(int i=0;i<10;i++){
         if(min_line[i].min==0){
-            flag=i;
+            line_flag=i;
             break;
         }
     }
@@ -93,24 +96,74 @@ int main(void){
     for(int i=0;i<line_number;i++){
         cout<<min_line[i].min<<" "<<min_line[i].start.x<<" "<<min_line[i].start.y<<" "<<min_line[i].end.x<<" "<<min_line[i].end.y<<endl;
     }
-    cout<<flag<<endl;
+    cout<<line_flag<<endl;
 
     //上の２点による線と最小の距離を取る点を探す
-    for(int i=0;i<flag;i++){
-        Vertex2D vertex;
-        if((min_line[i].start.x!=z[0].x&&min_line[i].start.y!=z[0].y)&&(min_line[i].end.x!=z[0].x&&min_line[i].end.y!=z[0].y)) vertex=z[0];
-        else if((min_line[i].start.x!=z[1].x&&min_line[i].start.y!=z[1].y)&&(min_line[i].end.x!=z[1].x&&min_line[i].end.y!=z[1].y)) vertex=z[1];
-        else vertex=z[2];
-        double min=Distance_DotAndLine(vertex,min_line[i].start,min_line[i].end);
+    
+    //min_tri.minの初期値の入力
+    Vertex2D vertex;
+    if((min_line[0].start.x!=z[0].x&&min_line[0].start.y!=z[0].y)&&(min_line[0].end.x!=z[0].x&&min_line[0].end.y!=z[0].y)) vertex=z[0];
+    else if((min_line[0].start.x!=z[1].x&&min_line[0].start.y!=z[1].y)&&(min_line[0].end.x!=z[1].x&&min_line[0].end.y!=z[1].y)) vertex=z[1];
+    else vertex=z[2];
+    
+    min_tri[0].min=Distance_DotAndLine(vertex,min_line[0].start,min_line[0].end);
+    min_tri[0].dot[0]=min_line[0].start;
+    min_tri[0].dot[1]=min_line[0].end;
+    min_tri[0].dot[2]=vertex;
+    min_tri[0].dot[3]=min_tri[0].dot[0];
+    int tri_number=1;
+    for(int i=0;i<line_flag;i++){
+        
         for(int j=1;j<M;j++){
             if((min_line[i].start.x==z[j].x&&min_line[i].start.y==z[j].y)||(min_line[i].end.x==z[j].x&&min_line[i].end.y==z[j].y)) continue;
-            if(min>Distance_DotAndLine(z[j],min_line[i].start,min_line[i].end))
+            if(min_tri[0].min>Distance_DotAndLine(z[j],min_line[i].start,min_line[i].end)&&Distance_DotAndLine(z[j],min_line[i].start,min_line[i].end)!=0){
+                min_tri[0].min=Distance_DotAndLine(z[j],min_line[i].start,min_line[i].end);
+                min_tri[0].dot[0]=min_line[i].start;
+                min_tri[0].dot[1]=min_line[i].end;
+                min_tri[0].dot[2]=z[j];
+                min_tri[0].dot[3]=min_tri[0].dot[0];
+
+                //いままでのmin_tri[1]-[9]の初期化
+                for(int k=1;k<=tri_number;k++) min_tri[k]=min_tri[9];
+            }
+            else if(min_tri[0].min==Distance_DotAndLine(z[j],min_line[i].start,min_line[i].end)){
+                min_tri[tri_number].min=Distance_DotAndLine(z[j],min_line[i].start,min_line[i].end);
+                min_tri[tri_number].dot[0]=min_line[i].start;
+                min_tri[tri_number].dot[3]=min_tri[tri_number].dot[0];
+                min_tri[tri_number].dot[1]=min_line[i].end;
+                min_tri[tri_number++].dot[2]=z[j];
+
+            };
         }
     }
 
+    //確認
+    for(int i=0;i<tri_number;i++){
+        cout<<min_tri[i].min<<" "<<min_tri[i].dot[0].x<<" "<<min_tri[i].dot[0].y<<" "<<min_tri[i].dot[1].x<<" "<<min_tri[i].dot[1].y
+        <<" "<<min_tri[i].dot[2].x<<" "<<min_tri[i].dot[2].y<<endl;
+    }
 
+    //全ての辺の長さを出す
+    double side_len[tri_number][3];
+    for(int i=0;i<tri_number;i++){
+        for(int j=0;j<3;j++){
+            side_len[i][j]=Distance_DotAndDot(min_tri[i].dot[j],min_tri[i].dot[j+1]);
+        }
+    }
+    
+    //確認
+    for(int i=0;i<tri_number;i++){
+        for(int j=0;j<3;j++){
+            cout<<"length"<<side_len[i][j]<<endl;
+        }
+    }
 
-
+    CmpEachSide(side_len[0]);
+    for(int i=0;i<tri_number;i++){
+        for(int j=0;j<3;j++){
+            cout<<"length"<<side_len[i][j]<<endl;
+        }
+    }
 }
 
 int seifu(void){
@@ -145,4 +198,43 @@ double Distance_DotAndDot(Vertex2D a,Vertex2D b){
     m=pow(a.x-b.x,2);
     n=pow(a.y-b.y,2);
     return sqrt(m+n);
+}
+
+double Area_Triangle(Vertex2D m,Vertex2D n,Vertex2D l){
+
+    /* 三角形の面積 */
+    double S, s;
+    /* 三角形の辺 */
+    double a, b, c;
+    
+    a=Distance_DotAndDot(m,n);
+    b=Distance_DotAndDot(n,l);
+    c=Distance_DotAndDot(l,m);
+    /* 三角形の面積を計算・出力 */
+    s = (a + b + c) / 2.0;
+    S  = sqrt(s*(s-a)*(s-b)*(s-c));
+    
+    return S;
+
+}
+
+void CmpEachSide(double a[]){
+    double t;
+    for(int i = 0; i < N-1; i++)
+    {
+        double min=a[i];
+        int s=i;
+        for(int j = i+1; j < N; j++)
+        {
+            if (a[j]<min) {
+                min=a[j];
+                s=j;
+            }
+            
+
+        }
+        t=a[i];
+        a[i]=min;
+        a[s]=t;
+    }
 }
